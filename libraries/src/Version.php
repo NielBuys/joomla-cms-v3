d045a1e4805782839b592b7e5bd7e2f301331317
Niel Buys <nielbuys@winems.co.za>
2025-02-26 12:09:36 +0200

    Version up 3.11.6

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -49,7 +49,7 @@ final class Version
 	 * @var    integer
 	 * @since  3.8.0
 	 */
-	const PATCH_VERSION = 5;
+	const PATCH_VERSION = 6;
 
 	/**
 	 * Extra release version info.
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '23 January 2025';
+	const RELDATE = '26 February 2025';
 
 	/**
 	 * Release time.
