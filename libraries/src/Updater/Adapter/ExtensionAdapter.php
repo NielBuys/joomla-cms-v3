994f0236e8c5b1d323c3c3dfea7e33aba7bdaf4f
Niel Buys <nielbuys@winems.co.za>
2025-06-17 14:06:56 +0200

    Deprecated Warning fixes

--- libraries/src/Updater/Adapter/ExtensionAdapter.php
+++ libraries/src/Updater/Adapter/ExtensionAdapter.php
@@ -325,9 +325,8 @@ class ExtensionAdapter extends UpdateAdapter
 		}
 
 		$this->xmlParser = xml_parser_create('');
-		xml_set_object($this->xmlParser, $this);
-		xml_set_element_handler($this->xmlParser, '_startElement', '_endElement');
-		xml_set_character_data_handler($this->xmlParser, '_characterData');
+		xml_set_element_handler($this->xmlParser, [$this, '_startElement'], [$this, '_endElement']);
+		xml_set_character_data_handler($this->xmlParser, [$this, '_characterData']);
 
 		if (!xml_parse($this->xmlParser, $response->body))
 		{
