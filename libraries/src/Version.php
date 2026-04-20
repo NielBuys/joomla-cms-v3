741b662c084a34fc1da3aa606e0edc6f76dc1ea4
Niel Buys <nbuys@ncomp.co.za>
2026-04-20 14:52:47 +0200

    Version up

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -49,7 +49,7 @@ final class Version
 	 * @var    integer
 	 * @since  3.8.0
 	 */
-	const PATCH_VERSION = 11;
+	const PATCH_VERSION = 12;
 
 	/**
 	 * Extra release version info.
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '13 January 2026';
+	const RELDATE = '20 April 2026';
 
 	/**
 	 * Release time.
